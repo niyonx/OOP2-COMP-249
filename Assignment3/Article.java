35827a7906cada3a4239aed7608f95d83ffa4913
Nigel Yong <kenkenysy@gmail.com>
2019-03-14 10:15:31 -0400

    Everything works! (for now)

--- Assignment3/Article.java
+++ Assignment3/Article.java
@@ -49,37 +49,58 @@ public class Article {
         this.year = myYear;
         this.number = myNumber;
 
-        author = author.substring(author.indexOf("{")+1);
-        author = author.substring(0,author.indexOf("}"));
+        if (!author.equals("")) {
+            author = author.substring(author.indexOf("author={") + 8);
+//            System.out.println(author);
+//            author = author.substring(0, author.indexOf("}"));
 
-        journal = journal.substring(journal.indexOf("{")+1);
-        journal = journal.substring(0,journal.indexOf("}"));
+            journal = journal.substring(journal.indexOf("{") + 1);
+//            journal = journal.substring(0, journal.indexOf("}"));
 
-        title = title.substring(title.indexOf("{")+1);
-        title = title.substring(0,title.indexOf("}"));
+            title = title.substring(title.indexOf("{") + 1);
+//            title = title.substring(0, title.indexOf("}"));
 
-        volume = volume.substring(volume.indexOf("{")+1);
-        volume = volume.substring(0,volume.indexOf("}"));
+            volume = volume.substring(volume.indexOf("{") + 1);
+//            volume = volume.substring(0, volume.indexOf("}"));
 
-        pages = pages.substring(pages.indexOf("{")+1);
-        pages = pages.substring(0,pages.indexOf("}"));
+            pages = pages.substring(pages.indexOf("{") + 1);
+//            pages = pages.substring(0, pages.indexOf("}"));
 
-        doi = doi.substring(doi.indexOf("{")+1);
-        doi = doi.substring(0,doi.indexOf("}"));
+            doi = doi.substring(doi.indexOf("{") + 1);
+//            doi = doi.substring(0, doi.indexOf("}"));
 
-        month = month.substring(month.indexOf("{")+1);
-        month = month.substring(0,month.indexOf("}"));
+            month = month.substring(month.indexOf("{") + 1);
+//            month = month.substring(0, month.indexOf("}"));
 
-        year = year.substring(year.indexOf("{")+1);
-        year = year.substring(0,year.indexOf("}"));
+            year = year.substring(year.indexOf("{") + 1);
+//            year = year.substring(0, year.indexOf("}"));
 
-        number = number.substring(number.indexOf("{")+1);
-        number = number.substring(0,number.indexOf("}"));
+            number = number.substring(number.indexOf("{") + 1);
+//            number = number.substring(0, number.indexOf("}"));
 
-        authorIEEE = author.replace("and", ",");
-        authorNJ = author.replace("and", "&");
-        authorACM = author.substring(0,author.indexOf("and"))+"et al.";
+            authorIEEE = author.replace("and", ",");
+            authorNJ = author.replace("and", "&");
+            authorACM = author.substring(0, author.indexOf("and")) + "et al.";
+        }
 //        System.out.println(authorIEEE);
+//
+//        author={T. Jackson and A. H. Peterson and N. Wang
+//                ,
+//                journal={IEEE Transactions on Computer Science
+//                        ,
+//                        title={Detecting Security Vulnerabilities in Binary Code
+//                                ,
+//                                year={2017
+//                                        ,
+//                                        volume={QQ
+//                                                ,
+//                                                number={85
+//                                                        ,
+//                                                        pages={1-12
+//                                                                ,
+//                                                                keywords={Security attacks;Binary code processing;Security error detection;Deep machine learning;Static analysis
+//                ,
+//                doi={14.2408/TCS.2017.4746889
 //        System.out.println(authorACM);
 //        System.out.println(authorNJ);
     }
--- Assignment3/AuthorBibCreator.java
+++ Assignment3/AuthorBibCreator.java
@@ -1,68 +1,72 @@
 import java.io.*;
 import java.util.Scanner;
-
 public class AuthorBibCreator {
     public static int processBibFiles(PrintWriter pwIEEE, PrintWriter pwACM, PrintWriter pwNJ, Scanner sc, String authorName){
-        String s;
-        String author, journal, title, volume, pages, doi, month, year, number;
-
-
+        String s="";
+        String author= "", journal= "", title= "", volume= "", pages= "", doi= "", month= "", year= "", number= "";
         int count=0;
-        Article[] articles = null;
+        Article[] articles = new Article[100];
         for (int i = 1; i <= 10; i++){
             try{
                 sc = new Scanner(new FileInputStream("Latex"+i+".bib"));
-
-//                while(sc.hasNextLine()){
-//                    s = sc.nextLine();
-//                    if (s.toLowerCase().contains(authorName.toLowerCase())) {
-//                        count++;
-//                    }
-//                    articles = new Article[100];
-//                }
-
-                articles = new Article[100];
-
-//                sc.useDelimiter("@ARTICLE");
-                int j = 0;
                 while(sc.hasNextLine()){
                     s = sc.nextLine();
                     if (s.toLowerCase().contains(authorName.toLowerCase())) {
-//                        System.out.println(s);
                         count++;
+                    }
+                }
+
+                sc.close();
+
+                sc = new Scanner(new FileInputStream("Latex"+i+".bib"));
+                articles = new Article[count];
+                System.out.println(count);
+                sc.useDelimiter("}");
+                int j = 0;
+                while(sc.hasNextLine()) {
+                    if (!s.toLowerCase().contains(authorName.toLowerCase())){
+                        s = sc.next();
+                    }
+                    System.out.println(s+"yyyyyyyyy");
+                    if (s.toLowerCase().contains(authorName.toLowerCase())) {
+                        System.out.println(s+"xxxxxxxxxxxxxxxxxx");
+//                        count++;
                         author = s;
-                        s = sc.nextLine();
-                        journal = s;
-                        s = sc.nextLine();
-                        title = s;
-                        s = sc.nextLine();
-                        year = s;
-                        s = sc.nextLine();
-                        volume = s;
-                        s = sc.nextLine();
-                        number = s;
-                        s = sc.nextLine();
-                        pages = s;
-                        s = sc.nextLine();
-                        s = sc.nextLine();
-                        doi = s;
-                        s = sc.nextLine();
-                        s = sc.nextLine();
-                        month = s;
+                        s = sc.next();
+                        while (!s.toUpperCase().contains("@ARTICLE{") && sc.hasNext()) {
+                            System.out.println(s+"oooooo");
+                            if (s.toLowerCase().contains("journal")) {
+                                journal = s;
+                            } else if (s.toLowerCase().contains("title")) {
+                                title = s;
+                            } else if (s.toLowerCase().contains("year")) {
+                                year = s;
+                            } else if (s.toLowerCase().contains("volume")) {
+                                volume = s;
+                            } else if (s.toLowerCase().contains("number")) {
+                                number = s;
+                            } else if (s.toLowerCase().contains("pages")) {
+                                pages = s;
+                            } else if (s.toLowerCase().contains("doi")) {
+                                doi = s;
+                            } else if (s.toLowerCase().contains("month")) {
+                                month = s;
+                            }
+                            s= sc.next();
+                            System.out.println(s+"kkkkkkkkkkkkkkk");
+                        }
                         Article a = new Article(author, journal, title, volume, pages, doi, month, year, number);
                         articles[j] = a;
                         j++;
                     }
                 }
-
                 for (int k = 0; k < articles.length; k++) {
-                    if (articles[k]!=null) {
+                    if (articles[k] != null) {
                         pwIEEE.println(articles[k].toStringIEEE());
                         pwACM.println(articles[k].toStringACM());
                         pwNJ.println(articles[k].toStringNJ());
                     }
                 }
-
             }catch(FileNotFoundException e){
                 System.out.println("Could not open input file Latex"+i+".bib for reading. Please check if file exists! Program will terminate after closing any opened files.");
                 System.exit(0);
@@ -73,16 +77,14 @@ public class AuthorBibCreator {
         pwNJ.close();
         return count;
     }
-
     public static void main(String[] args) {
         String authorName;
+        int matches=0;
         Scanner keyIn = new Scanner(System.in);
         Scanner sc = null;
-
-        System.out.println("Welcome to BibCreator!");
+        System.out.println("\nWelcome to BibCreator!\n");
         System.out.print("Please enter the author name you are targeting: ");
         authorName = keyIn.nextLine();
-
         System.out.println();
         for (int i = 1; i <= 10; i++){
             try{
@@ -93,38 +95,44 @@ public class AuthorBibCreator {
                 System.exit(0);
             }
         }
-
         try{
             File fIEEE = new File(authorName+"-IEEE.json"), fACM = new File(authorName+"-ACM.json"), fNJ = new File(authorName+"-NJ.json");
             if (fIEEE.exists()||fACM.exists()||fNJ.exists()){
-                throw new FileExistsException();
+                throw new FileExistsException("A file already exists with the name: "+authorName+"-IEEE-BU.json\n"+
+                        "File will be renamed as: "+authorName+"-IEEE-BU.json and any old BUs will be deleted.\n\n"+
+                        "A file already exists with the name: "+authorName+"-ACM-BU.json\n"+
+                        "File will be renamed as: "+authorName+"-ACM-BU.json and any old BUs will be deleted.\n\n"+
+                        "A file already exists with the name: "+authorName+"-NJ-BU.json\n"+
+                        "File will be renamed as: "+authorName+"-NJ-BU.json and any old BUs will be deleted.\n");
             }
         }catch (FileExistsException e){
             System.out.println(e.getMessage());
             File fIEEE = new File(authorName+"-IEEE-BU.json"), fACM = new File(authorName+"-ACM-BU.json"), fNJ = new File(authorName+"-NJ-BU.json");
             if (fIEEE.exists()){
+//                System.out.println("A file already exists with the name: "+authorName+"-IEEE-BU.json");
+//                System.out.println("File will be renamed as: "+authorName+"-IEEE-BU.json and any old BUs will be deleted.\n");
                 fIEEE.delete();
             }
             File fIEEEBU = new File(authorName+"-IEEE.json");
-            fIEEEBU.renameTo( new File(authorName+"-IEEE-BU.json"));
-
+            fIEEEBU.renameTo( new File(authorName+"-IEEE-BU.json\n"));
             if (fACM.exists()){
+//                System.out.println("A file already exists with the name: "+authorName+"-ACM-BU.json");
+//                System.out.println("File will be renamed as: "+authorName+"-ACM-BU.json and any old BUs will be deleted.\n");
                 fACM.delete();
             }
             File fACMBU = new File(authorName+"-ACM.json");
             fACMBU.renameTo( new File(authorName+"-ACM-BU.json"));
-
             if (fNJ.exists()){
+//                System.out.println("A file already exists with the name: "+authorName+"-NJ-BU.json");
+//                System.out.println("File will be renamed as: "+authorName+"-NJ-BU.json and any old BUs will be deleted.");
                 fNJ.delete();
             }
             File fNJBU = new File(authorName+"-NJ.json");
             fNJBU.renameTo( new File(authorName+"-NJ-BU.json"));
         }
-
         // generate three files
         PrintWriter pwIEEE = null, pwACM = null, pwNJ = null;
         Boolean opened = true;
-
         try {
             pwIEEE = new PrintWriter(new FileOutputStream(authorName + "-IEEE.json"));
         } catch (FileNotFoundException e) {
@@ -132,7 +140,6 @@ public class AuthorBibCreator {
             opened = false;
             e.printStackTrace();
         }
-
         try {
             pwACM = new PrintWriter(new FileOutputStream(authorName + "-ACM.json"));
         } catch (FileNotFoundException e) {
@@ -140,7 +147,6 @@ public class AuthorBibCreator {
             opened = false;
             e.printStackTrace();
         }
-
         try {
             pwNJ = new PrintWriter(new FileOutputStream(authorName + "-NJ.json"));
         } catch (FileNotFoundException e) {
@@ -148,14 +154,20 @@ public class AuthorBibCreator {
             opened = false;
             e.printStackTrace();
         }
-
-        if (!opened){
+        matches = processBibFiles(pwIEEE,pwACM,pwNJ,sc,authorName);
+        if (!opened || matches ==0){
             File fIEEE = new File(authorName + "-IEEE.json"), fACM = new File(authorName + "-ACM.json"), fNJ = new File(authorName + "-NJ.json");
             fIEEE.delete();
             fACM.delete();
             fNJ.delete();
         }
-
-        System.out.println("A total of "+processBibFiles(pwIEEE,pwACM,pwNJ,sc,authorName)+" records were found for author(s) with name: Park");
+        if (matches == 0){
+            System.out.println("No records were found for author(s) with name: "+authorName);
+            System.out.println("No files have been created!");
+        }else{
+            System.out.println("A total of "+matches+" records were found for author(s) with name: "+authorName);
+            System.out.println("Files "+authorName + "-IEEE.json, "+authorName + "-ACM.json and"+ authorName + "-NJ.json have been created!");
+        }
+        System.out.println("\nGoodbye! Hope you have enjoyed creating the needed files using AuthorBibCreated!");
     }
 }
